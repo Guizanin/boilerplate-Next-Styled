5f3b6143d98c0ebd6c8f83f0a6be9c2fd06da7d4
Guizanin <guilhermecasazanin@gmail.com>
2021-06-15 22:19:46 -0300

    start structure

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import { Teste } from '../styles/styles'
+
+export default function Home() {
+	return (
+		<div>
+			Hello world
+			<Teste>teste</Teste>
+			<h3></h3>
+			<h3></h3>
+			<h3></h3>
+			<h3></h3>
+		</div>
+	)
+}
