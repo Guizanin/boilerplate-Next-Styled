377cec6e4f12f53c35acdc08e03d6a0925bb6e74
guilherme zanin <guilhermecasazanin@gmail.com>
2021-08-16 21:59:04 -0300

    add tagmanager e keywords

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -39,6 +39,21 @@ export default class MyDocument extends Document {
     return (
       <Html lang='pt-BR'>
         <Head>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-XXXXXXX');`
+            }}
+          ></script>
+          <meta
+            name='keywords'
+            content='palavras chaves'
+          />
+          <meta name='author' content='Autor' />
+
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
             href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap'
@@ -47,6 +62,12 @@ export default class MyDocument extends Document {
           <link rel='shortcut icon' href='/favicon.png' type='image/png' />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
